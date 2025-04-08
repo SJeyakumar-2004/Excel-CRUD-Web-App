28c61f13e97e1a9ee2a773a60460e4303eed6d0a
S.Jeyakumar <149045275+SJeyakumar-2004@users.noreply.github.com>
2025-04-08 17:15:42 +0530

    Add files via upload

---
+++ Exc/src/main/java/com/example/Exc/ExcApplication.java
@@ -0,0 +1,12 @@
+package com.example.Exc;
+
+
+
+public class ExcApplication {
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
---
+++ Exc/src/test/java/com/example/Exc/ExcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
